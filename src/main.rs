c357837a4fc0127e9c88c20bdb7295200500fba9
Bruno Pessoa <37255851+BrunoPessoa097@users.noreply.github.com>
2022-12-16 01:56:13 -0300

    programa de conversao de temperatura celsius e fahrenheit

---
+++ src/main.rs
@@ -0,0 +1,65 @@
+use core::f32;
+use std::io;
+
+// função fahrenheit
+fn fahrenheit(t:f32) -> f32{
+    (t * 1.8)+32.0
+}
+
+// função celsius
+fn celsius(t:f32) -> f32{
+    (t-32.00)/1.8
+}
+
+fn main() {
+    loop {
+        println!("converta o em celsius para fahrenheit");
+
+        // input opção usuário.
+        println!("Escolha o tipo da conversão:");
+        println!("Digite 1-Celsius");
+        println!("Digite 2-Fahrenheit");
+        println!("Digite 3-finalizar o programa");
+
+        let mut esc = String::new();
+
+        io::stdin()
+                .read_line(&mut esc)
+                .expect("Escolha errada");
+
+        let _esc:f32 = match esc.trim().parse() {
+            Ok(num) => num,
+            Err(_e) => {
+                -1.00
+            }
+        };
+
+        // input temperatura usuário
+        println!("Digite a temperatura: ");
+        let mut temperatura = String::new();
+
+        io::stdin()
+            .read_line(&mut temperatura)
+            .expect("Temperatura errada");
+
+        
+        let temperatura:f32 = match temperatura.trim().parse() {
+            Ok(num) => num,
+            Err(_e) => {
+                -1.00
+            }
+        };
+
+        if _esc == 1.0{
+            let c:f32 = celsius(temperatura);
+            println!("temperatura em celsius: {c}");
+            break;
+        }else if _esc ==2.0{
+            let f:f32 = fahrenheit(temperatura);
+            println!("temperatura em fahrenheit: {f}");
+            break;
+        }else{
+            break;
+        };
+    }
+}
